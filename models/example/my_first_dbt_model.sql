15ff79d62d9ecdae01acaba28d2d757a7fc427da
devans561@gmail.com <devans561@gmail.com>
2026-09-17 19:46:52 +0000

    init

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,11 +1,5 @@
 
-
-  create or replace table `code-breakfast`.`dbt_misja`.`my_first_dbt_model`
-  
-  
-  OPTIONS()
-  as (
-    /*
+/*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
@@ -13,7 +7,7 @@
     Try changing "table" to "view" below
 */
 
-
+{{ config(materialized='table') }}
 
 with source_data as (
 
@@ -31,4 +25,3 @@ from source_data
 */
 
 -- where id is not null
-  );
