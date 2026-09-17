968b2289365a5816d4f314c957ed4df10b3b7e97
Your Name <you@example.com>
2026-09-17 16:07:56 -0400

    Add orders

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,11 +1,4 @@
-
-
-  create or replace table `code-breakfast`.`dbt_misja`.`my_first_dbt_model`
-  
-  
-  OPTIONS()
-  as (
-    /*
+ /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
