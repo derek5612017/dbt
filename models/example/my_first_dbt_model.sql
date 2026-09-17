4deb6ad7d69be0a5ba5399033d41cc0c0c48d4ed
derek5612017 <32799054+derek5612017@users.noreply.github.com>
2026-09-17 15:00:07 -0400

    Modify my_first_dbt_model to count records from source_data
    Updated the SQL model to select from bronze_orders and count records.

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -11,13 +11,12 @@
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+    select * 
+    from workspace.default.bronze_orders;
 
 )
 
- select * from workspace.default.bronze_orders;
+ select count(*) from source_data;
 
 /*
     Uncomment the line below to remove records with null `id` values
