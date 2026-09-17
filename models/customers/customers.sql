ccbe0a88d7c7108ad06e6b33233c4684045624ee
Your Name <you@example.com>
2026-09-17 17:51:28 -0400

    switch to orders

---
+++ models/customers/customers.sql
@@ -0,0 +1,10 @@
+-- customers.sql
+{{ config(materialized='table') }}
+
+select  distinct 
+    customer_id
+    ,customer_name
+    ,city
+    ,state_or_province as state, 
+    postal_code 
+from workspace.default.bronze_orders where row_id is not null
